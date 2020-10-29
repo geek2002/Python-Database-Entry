f0cbc1198e101ea28ad432b079d4f699927fd8f9
577171 <71441597+577171@users.noreply.github.com>
2020-10-29 23:57:19 +0000

    Finished Creating Tables test file

--- create table.py
+++ create table.py
@@ -4,10 +4,24 @@ import json
 with open('tables copy.json', 'r') as f:
         tables_dict = json.load(f)
 
+
 conn = pypyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\Libraries\Documents\Microsoft Access\Test Database.accdb;')
 cursor = conn.cursor()
+#cursor.execute("CREATE TABLE Persons (Personid AUTOINCREMENT PRIMARY KEY,LastName varchar(255) NOT NULL,FirstName varchar(255),Age int);")
 
-",".join(tables_dict["customer"][""])
-cursor.execute("CREATE TABLE Persons (Personid AUTOINCREMENT PRIMARY KEY,LastName varchar(255) NOT NULL,FirstName varchar(255),Age int);")
-conn.commit()
-
+def createTable():
+        sql="CREATE TABLE"
+        for x in tables_dict:
+                sql=sql + " " + x + " (" + x + "_id AUTOINCREMENT PRIMARY KEY,"
+                feilds=tables_dict[x]["feilds"]
+                dataTypes=tables_dict[x]["dataTypes"]
+                y=0
+                for y in range(len(feilds)):
+                        if y != (len(feilds)-1):
+                                sql=sql + feilds[y] + " " + dataTypes[y] + " , "
+                        else:
+                                sql=sql + " " + feilds[y] + " " + dataTypes[y] + ") "
+                cursor.execute(sql)
+                print(sql)
+                sql="CREATE TABLE"
+        conn.commit()
