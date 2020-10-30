7f0eabfe9e4fd210bf2210a88202e9ef4a3ae4a8
577171 <71441597+577171@users.noreply.github.com>
2020-10-30 02:25:17 +0000

    Update main UI.py
    -Added support for changing location while running
    -Added checks for if the file exists
    -Added testPyodbc method

--- main UI.py
+++ main UI.py
@@ -10,11 +10,58 @@ namesUsed=[]
 keys=[]
 tables_dict={}
 
+# --------- Check Files  ---------
+def changeLocation():
+    print("Database File Not Found")
+    print("Please enter Database File Location")
+    databaseFile=input(">> ")
+    locaFile=open("databaseLocation.txt","w")
+    locaFile.write(databaseFile)
+    locaFile.close()
+    return databaseFile
+
+if os.path.exists("databaseLocation.txt"):
+    file=open("databaseLocation.txt","r")
+    databaseFile=file.readlines()[0]
+    file.close()
+    fileExists = os.path.exists(databaseFile)
+    if fileExists == False:
+        changeLocation()
+else:
+    databaseFile=changeLocation()
+
+
 # --------- Link Access Driver  ---------
-conn = pypyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\Libarys\Documents\Microsoft Access\Test Database.accdb;')
+conn = pypyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+databaseFile+';')
 cursor = conn.cursor()
 
 # --------- Functions ---------
+def Menu():
+    print("")
+    print("╔═══════════════════════════════════════╗")
+    print("║          1:Enter Names                ║")
+    print("║          2:Enter Cards                ║")
+    print("║          3:Enter Sales                ║")
+    print("║          4:Get Tables                 ║")
+    print("║          5:Exit                       ║")
+    print("╚═══════════════════════════════════════╝")
+    do=input("> ")
+    if do == "1":
+        clean()
+        addNames(int(input("How Many? >> ")))
+    elif do == "2":
+        clean()
+        Menu()
+    elif do == "3":
+        clean()
+        Menu()
+    elif do == "4":
+        getTables()
+    elif do == "5":
+        exit()
+    else:
+        print("Error , Please only use 1,2,3 or 4")
+        Menu()
 def log(data):
     print(data)
 def clean():
@@ -23,9 +70,7 @@ def loadJSON():
     with open('tables.json', 'r') as f:
         tables_dict = json.load(f)
         for x in tables_dict:
-            print(x)
             keys.append(x)
-        print(tables_dict)
     return tables_dict
 def convertDate(date):
     date=date.split("/")
@@ -92,29 +137,42 @@ def writeToDatabase(tableIndex,data,commit=False):
     cursor.execute(sql)
     if commit:
         conn.commit()
-def Menu():
-    print("")
-    print("╔═══════════════════════════════════════╗")
-    print("║          1:Enter Names                ║")
-    print("║          2:Enter Cards                ║")
-    print("║          3:Enter Sales                ║")
-    print("║          4:Exit                       ║")
-    print("╚═══════════════════════════════════════╝")
-    do=input("> ")
-    if do == "1":
-        clean()
-        addNames(int(input("How Many? >> ")))
-    elif do == "2":
-        clean()
-        Menu()
-    elif do == "3":
-        clean()
-        Menu()
-    elif do == "4":
-        exit()
-    else:
-        print("Error , Please only use 1,2,3 or 4")
-        Menu()
+def testPyodbc():
+    try:
+        import pyodbc
+        libary = True
+    except ModuleNotFoundError:
+        print("pyodbc libary not installed please run 'pip install pyodbc' to install")
+        libary = False
+    
+    try:
+        pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+databaseFile+';')
+        driver = True
+    except pyodbc.InterfaceError:
+        print("Driver Error")
+        print("Unable to create tables automatically")
+        print("Please create tables manually")
+        driver=False
+    except pyodbc.Error:
+        if os.path.exists(databaseFile):
+            print("Unknown Error Occured")
+            driver=False
+            
+        else:
+            print("File Not Found")
+            driver=False
+    return libary,driver
+def getTables():
+    curs = conn.cursor()
+    tables = []
+    for row in curs.tables():
+        tableName=row.table_name
+        if "MS" not in tableName:
+            tables.append(tableName)
+            print(row.table_name)
+    cursor.close()
+    return tables
+    
 
 tables_dict=loadJSON()
 Menu()
