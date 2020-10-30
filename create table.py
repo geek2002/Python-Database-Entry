600b9b6474b7ac87d6ff1ff7017305ac48ae58e2
577171 <71441597+577171@users.noreply.github.com>
2020-10-30 02:25:28 +0000

    Update create table.py

--- create table.py
+++ create table.py
@@ -5,7 +5,7 @@ with open('tables copy.json', 'r') as f:
         tables_dict = json.load(f)
 
 
-conn = pypyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\Libraries\Documents\Microsoft Access\Test Database.accdb;')
+conn = pypyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\Libarys\Documents\Microsoft Access\Test Database.accdb;')
 cursor = conn.cursor()
 #cursor.execute("CREATE TABLE Persons (Personid AUTOINCREMENT PRIMARY KEY,LastName varchar(255) NOT NULL,FirstName varchar(255),Age int);")
 
@@ -25,3 +25,5 @@ def createTable():
                 print(sql)
                 sql="CREATE TABLE"
         conn.commit()
+
+createTable()
