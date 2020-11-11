87142474e8a9a6b848965ea300a1e100a54e730e
577171 <71441597+577171@users.noreply.github.com>
2020-11-11 00:58:37 +0000

    Create Check CSV.py
    Script to check the perform tasks with the main Names.csv file

---
+++ Check CSV.py
@@ -0,0 +1,35 @@
+import csv
+import os
+def convertDate(date):
+    date=date.split("/")
+    Day = date[1].zfill(2)
+    Month = date[0].zfill(2)
+    Year = date [2].zfill(2)
+    date = Day + "/" + Month + "/" + Year
+    return date
+def formatCSV():
+    f=open("Names.csv", encoding='utf-8-sig')
+    n=open("Names New.csv","a", encoding='utf-8-sig')
+    lines = f.readlines()
+
+    for line in lines:
+        row=line.split(",")
+        row[5]=convertDate(row[5])
+        line=",".join(row)
+        n.write(line)
+        num=row[0][0:2]
+        print(num + " %")
+    n.close()
+def checkLineLengths():
+    file=open("Names.csv", encoding='utf-8-sig')
+    lines = file.readlines()
+    errors=[]
+    for line in lines:
+        row=line.split(",")
+        num=row[0][0:2]
+        print(num + " %")
+        if len(row) != 9:
+            errors.append(row[0])
+    print(errors)
+#checkLineLengths()
+formatCSV()
